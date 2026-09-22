cf60cc09fd54944867648c291a5af4f74615cbfe
Ivan-Tsarikov <i.i.tsarikov@gmail.com>
2026-09-22 15:27:54 +0300

    HW2 fix2

--- content-based-movie-recommender/tests/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-project_dir=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
-
-osascript -l JavaScript "$project_dir/tests/app-tests.jxa" "$project_dir"
-python3 -m unittest discover -s "$project_dir/tests" -p "test_*.py" -v
-
